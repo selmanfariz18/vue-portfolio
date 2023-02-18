b08508c60abca5fafee36c4f207bf973d5f4404b
selmanfariz18 <salmanulfarizyka@gmail.com>
2023-02-18 15:56:12 +0530

    basic front created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,45 @@
   <v-app id="home">
     <NavBar />
     <v-container fluid>
-
+      <div class="head">
+        <v-row>
+          <v-col cols="5">
+            <div style="position: relative" class="mt-16">
+            <h1 class="text-grey">Hello,</h1>
+            <h1 class="text-white">I M SELMANUL FARIZY KA</h1>
+            <span class="text-grey">Vue developer</span><br/>
+            <v-btn tile dark class="text-red mt-8" variant="outlined">Contact Me</v-btn>
+          </div>
+          </v-col>
+          <v-col cols="2">
+            <div style="position:absolute;z-index: 9999;bottom:0;margin-left: auto;margin-right: auto;left:0;right: 0;text-align:center ;">
+              <v-icon>fas fa-angle-double-down</v-icon>
+            </div>
+          </v-col>
+          <v-col clos="5">
+            <div style="position: relative;z-index: 9999;" class="mt-16">
+              <v-img src="../../public/mine.png" contain max-height="300"></v-img>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+      <v-col cols="12" class="mt-16" id="about">
+        <div>
+          <v-row>
+            <v-col cols="12" sm="6">
+              <div class="egg">
+                <v-img src="../../public/mine.png" max-height="300"></v-img>
+              </div>
+            </v-col>
+            <v-col cols="12" sm="6">
+              <h5 class="mt-16">About Me</h5>
+              <div style="width:120px">
+              <v-slider v-model="slider2" color="red" :label="track-color"></v-slider>
+              </div>
+            </v-col>
+          </v-row>
+        </div>
+      </v-col>
     </v-container>
   </v-app>
 </template>
@@ -15,9 +53,77 @@ import NavBar from '@/components/NavBar.vue';
 
 export default defineComponent({
   name: 'HomeView',
+  setup(){
+    return {
+      slider2:50,
+      items:[
+        {
+          img:"../../public/mine1.png",
+        },
+        {
+          img:"../../public/mine2.png",
+        },
+        {
+          img:"../../public/mine3.png",
+        },
+        {
+          img:"../../public/mine3.png",
+        },
+        {
+          img:"../../public/mine4.png",
+        },
+        {
+          img:"../../public/mine5.png",
+        },
+      ],
+    };
+  },
 
   components: {
     NavBar,
   },
 });
 </script>
+<style scoped>
+.v-container{
+  padding:16px 0;
+}
+.head{
+  position: relative;
+  text-align: center;
+  padding: 12px;
+  margin-bottom: 6px;
+  height: 400px;
+  width:100%;
+  color: white;
+}
+.head:before{
+  content: " ";
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width:50%;
+  background: black;
+  transform: skew(0deg,6deg);
+}
+.head::after{
+  content: " ";
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 100%;
+  width:50%;
+  background: black;
+  transform: skew(0deg,-6deg);
+}
+.egg{
+  display: block;
+  margin-left: 100px;
+  margin-top: 50px;
+  width: 356px;
+  height: 300px;
+  background-color: #f04141;
+  border-radius: 50% 50% 50% 50% / 60% 60% 40% 40%;
+}
+</style>
